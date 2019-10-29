a34884c45ec9d1cb5d1204730b7d94a2e2e27b41
Glitch (fcc-req-header-parser-proj) <none>
2019-10-29 09:49:55 +0000

    Exporting to SunnyJohal/fcc-header-parser-microservice

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// Middlewares
+app.use(cors({ optionSuccessStatus: 200 }));
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", (req, res) => {
+  res.json({ greeting: "hello API" });
+});
+
+// Whoami endpoint...
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    ipaddress: req.headers["x-forwarded-for"],
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
